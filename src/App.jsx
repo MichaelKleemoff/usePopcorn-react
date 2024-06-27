ec431eb53e22563339846b855d0a1df3f91ebae8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-06-27 06:36:19 -0400

    Create 'Search' component in 'App.jsx'.

--- src/App.jsx
+++ src/App.jsx
@@ -68,17 +68,23 @@ function NavBar() {
 				<span role='img'>🍿</span>
 				<h1>usePopcorn</h1>
 			</div>
-			<input
-				className='search'
-				type='text'
-				placeholder='Search movies...'
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
+
 			<p className='num-results'>
 				Found <strong>X</strong> results
 			</p>
 		</nav>
+	);
+}
+
+function Search() {
+	return (
+		<input
+			className='search'
+			type='text'
+			placeholder='Search movies...'
+			value={query}
+			onChange={(e) => setQuery(e.target.value)}
+		/>
 	);
 }
 
