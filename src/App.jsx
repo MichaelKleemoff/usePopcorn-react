0f8dbc11411ac1721f11f132333be5b0a3d86f16
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-06-27 09:19:35 -0400

    Create 'WatchedSummary' in 'App.jsx'. Pass 'watched' prop to 'WatchedSummary' and move 3 derives states in.

--- src/App.jsx
+++ src/App.jsx
@@ -131,10 +131,6 @@ function WatchedBox() {
 
 	const [isOpen2, setIsOpen2] = useState(true);
 
-	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-	const avgUserRating = average(watched.map((movie) => movie.userRating));
-	const avgRuntime = average(watched.map((movie) => movie.runtime));
-
 	return (
 		<div className='box'>
 			<button
@@ -145,28 +141,6 @@ function WatchedBox() {
 			</button>
 			{isOpen2 && (
 				<>
-					<div className='summary'>
-						<h2>Movies you watched</h2>
-						<div>
-							<p>
-								<span>#️⃣</span>
-								<span>{watched.length} movies</span>
-							</p>
-							<p>
-								<span>⭐️</span>
-								<span>{avgImdbRating}</span>
-							</p>
-							<p>
-								<span>🌟</span>
-								<span>{avgUserRating}</span>
-							</p>
-							<p>
-								<span>⏳</span>
-								<span>{avgRuntime} min</span>
-							</p>
-						</div>
-					</div>
-
 					<ul className='list'>
 						{watched.map((movie) => (
 							<li key={movie.imdbID}>
@@ -219,5 +193,35 @@ function Movie({ movie }) {
 				</p>
 			</div>
 		</li>
+	);
+}
+
+function WatchedSummary({ watched }) {
+	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+	const avgUserRating = average(watched.map((movie) => movie.userRating));
+	const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+	return (
+		<div className='summary'>
+			<h2>Movies you watched</h2>
+			<div>
+				<p>
+					<span>#️⃣</span>
+					<span>{watched.length} movies</span>
+				</p>
+				<p>
+					<span>⭐️</span>
+					<span>{avgImdbRating}</span>
+				</p>
+				<p>
+					<span>🌟</span>
+					<span>{avgUserRating}</span>
+				</p>
+				<p>
+					<span>⏳</span>
+					<span>{avgRuntime} min</span>
+				</p>
+			</div>
+		</div>
 	);
 }
