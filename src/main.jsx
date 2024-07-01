9e1f28ad77f3dfc76005bd0bd565af1bbf87b28f
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-01 13:49:48 -0400

    Add a 'defaultRating' prop with an inital value. Implement it within 'StarRating'.

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 			maxRating={5}
 			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
 		/>
-		<StarRating size={24} color='red' className='test' />
+		<StarRating size={24} color='red' className='test' defaultRating={3} />
 	</React.StrictMode>
 );
