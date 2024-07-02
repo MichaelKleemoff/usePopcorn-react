1be9ac41bf104ec089b85a75232974fccfd9661c
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-02 05:43:09 -0400

    Accept 'onSetRating' prop in 'StarRating'.

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -18,6 +18,7 @@ const StarRating = ({
 	className = '',
 	messages = [],
 	defaultRating = 0,
+	onSetRating,
 }) => {
 	const [rating, setRating] = useState(defaultRating);
 	const [tempRating, setTempRating] = useState(0);
