ea98a08f4e040695ff7e42a42b88a77f54dbef41
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-02 05:32:45 -0400

    Create 'Test' component in 'main.jsx'.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx'
 // import './index.css'
 
 import StarRating from './StarRating';
+
+function Test() {
+	const [movieRating, setMovieRating] = useState(0);
+
+	return (
+		<div>
+			<StarRating color='blue' maxRating={10} />
+			<p>This movie was rated {movieRating} stars</p>
+		</div>
+	);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
@@ -13,5 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 			messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
 		/>
 		<StarRating size={24} color='red' className='test' defaultRating={3} />
+
+		<Test />
 	</React.StrictMode>
 );
