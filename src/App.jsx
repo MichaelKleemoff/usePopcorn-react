deac6e1e0c8b6cfc13f8ccb9efec2dc1ba33d818
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-10 09:20:53 -0400

    Reset 'movies', 'error' hooks before calling 'fetchMovies' based on condition.

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,13 @@ export default function App() {
 				setIsLoading(false);
 			}
 		}
+
+		if (query.length < 3) {
+			setMovies([]);
+			setError('');
+			return;
+		}
+
 		fetchMovies();
 	}, [query]);
 
