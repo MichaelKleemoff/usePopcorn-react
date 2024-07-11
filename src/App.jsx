cbdc974d7c2ca2b220b66a4cbaeb6920b1fa0d54
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-11 08:48:37 -0400

    Implement 'useEffect' and create/implement 'getMovieDetails' async function inside.

--- src/App.jsx
+++ src/App.jsx
@@ -233,6 +233,17 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+	useEffect(() => {
+		async function getMovieDetails() {
+			const res = await fetch(
+				`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+			);
+			const data = await res.json();
+			console.log(data);
+		}
+		getMovieDetails();
+	}, []);
+
 	return (
 		<div className='details'>
 			<button className='btn-back' onClick={onCloseMovie}>
