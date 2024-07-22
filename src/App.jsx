fddc8372239ab4de42afb72aa7787c89489b7fd0
Michael Kleemoff <michaelkleemoff2@gmail.com>
2024-07-22 10:19:21 -0400

    Create/implement 'isLoading' hook with conditional rendering in 'MovieDetails'.

--- src/App.jsx
+++ src/App.jsx
@@ -235,6 +235,7 @@ function Movie({ movie, onSelectMovie }) {
 
 function MovieDetails({ selectedId, onCloseMovie }) {
 	const [movie, setMovie] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 
 	// Destructure the 'movie' object -
 	const {
@@ -252,11 +253,13 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 
 	useEffect(() => {
 		async function getMovieDetails() {
+			setIsLoading(true);
 			const res = await fetch(
 				`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
 			);
 			const data = await res.json();
 			setMovie(data);
+			setIsLoading(false);
 			console.log(data);
 		}
 		getMovieDetails();
@@ -264,34 +267,40 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 
 	return (
 		<div className='details'>
-			<header>
-				<button className='btn-back' onClick={onCloseMovie}>
-					&larr;
-				</button>
-				<img src={poster} alt={`Poster of ${title} movie`} />
-				<div className='details-overview'>
-					<h2>{title}</h2>
-					<p>
-						{released} &bull; {runtime}
-					</p>
-					<p>{genre}</p>
-					<p>
-						<span>⭐️</span>
-						{imdbRating} IMDb rating
-					</p>
-				</div>
-			</header>
+			{isLoading ? (
+				<Loader />
+			) : (
+				<>
+					<header>
+						<button className='btn-back' onClick={onCloseMovie}>
+							&larr;
+						</button>
+						<img src={poster} alt={`Poster of ${title} movie`} />
+						<div className='details-overview'>
+							<h2>{title}</h2>
+							<p>
+								{released} &bull; {runtime}
+							</p>
+							<p>{genre}</p>
+							<p>
+								<span>⭐️</span>
+								{imdbRating} IMDb rating
+							</p>
+						</div>
+					</header>
 
-			<section>
-				<div className='rating'>
-					<StarRating maxRating={10} size={24} />
-				</div>
-				<p>
-					<em>{plot}</em>
-				</p>
-				<p>Starring {actors}</p>
-				<p>Directed by {director}</p>
-			</section>
+					<section>
+						<div className='rating'>
+							<StarRating maxRating={10} size={24} />
+						</div>
+						<p>
+							<em>{plot}</em>
+						</p>
+						<p>Starring {actors}</p>
+						<p>Directed by {director}</p>
+					</section>
+				</>
+			)}
 		</div>
 	);
 }
